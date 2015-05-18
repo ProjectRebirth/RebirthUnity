015bcb961c72753270c818f8524b7f42b9aee2e0
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-05-18 19:52:56 -0400

    Minor updates and some unimplemented scripts added

---
+++ RebirthUnity/Assets/Scripts/AIScripts/State.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Not implemented
+public abstract class State {
+
+	public abstract void enterState ();
+
+	public abstract void updateState ();
+
+	public abstract void exitState();
+}
---
+++ RebirthUnity/Assets/Scripts/AIScripts/StateMachine.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMachine : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void changeState() {
+
+	}
+}
