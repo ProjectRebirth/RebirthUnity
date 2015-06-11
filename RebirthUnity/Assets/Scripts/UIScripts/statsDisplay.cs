d38a715eff08cee6b89145b936185e47c1c32113
Raymond Hua <rymndhua@gmail.com>
2015-06-11 17:31:32 -0400

    Fixed Fill Bar
    Yay Ryan

--- RebirthUnity/Assets/Scripts/UIScripts/statsDisplay.cs
+++ RebirthUnity/Assets/Scripts/UIScripts/statsDisplay.cs
@@ -24,17 +24,41 @@ public class statsDisplay : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.O)) {
-			playerStats.damagePlayer(8f);
+			playerStats.damagePlayer(11f);
 		}
-		
+		float displacement1 = moveTowards ((initShield / playerStats.maxShield), (playerStats.curShield/ playerStats.maxShield), Time.deltaTime * .4f );
+		float displacement2 = moveTowards (initHealth / playerStats.maxHealth, playerStats.curHealth / playerStats.maxHealth, Time.deltaTime *.4f);
 		shieldBar.fillAmount = playerStats.curShield / playerStats.maxShield;
-		impactBar.fillAmount = Mathf.MoveTowards ((initShield / playerStats.maxShield), (playerStats.curShield/ playerStats.maxShield), Time.deltaTime * .000001f);
+		impactBar.fillAmount = displacement1;
 		healthBar.fillAmount = playerStats.curHealth / playerStats.maxHealth;
-		damageBar.fillAmount = Mathf.MoveTowards (initHealth / playerStats.maxHealth, playerStats.curHealth / playerStats.maxHealth, Time.deltaTime * .005f);
+		damageBar.fillAmount = displacement2;
 		
 		//shieldBar.fillAmount = Mathf.MoveTowards ((playerStats.curShield / playerStats.maxShield), 0, Time.smoothDeltaTime * .5f);
-		
-		initHealth = playerStats.curHealth;
-		initShield = playerStats.curShield;
+		initShield = displacement1 * playerStats.maxShield;
+		initHealth = displacement2 * playerStats.maxHealth;
+		// = playerStats.curHealth;
+		//initShield = playerStats.curShield;
 	}
+
+	float moveTowards(float current, float goal, float difference) {
+		if (current < goal) {
+			float newTime = current + difference;
+			if (newTime > goal) {
+				return goal;
+			} else {
+				return newTime;
+			}
+		} else if (current > goal) {
+			float newTime = current - difference;
+			if (newTime < goal) {
+				return goal;
+			} else {
+				return newTime;
+			}
+		} else {
+			return goal;
+		}
+
+	}
+
 }
