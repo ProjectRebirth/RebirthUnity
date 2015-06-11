cf62f5b5ee9eeb533620b9395f97d091e3000715
Raymond Hua <rymndhua@gmail.com>
2015-06-11 13:44:41 -0400

    UI Update 2.2
    Shell and Stuff

--- RebirthUnity/Assets/Scripts/UIScripts/playerStats.cs
+++ RebirthUnity/Assets/Scripts/UIScripts/playerStats.cs
@@ -1,87 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
-
-
 
 public class playerStats : MonoBehaviour {
-
+	
 	public float curShield;
 	public float maxShield;
-	public float initHealth;
-	public float initShield;
+	
 	public float curHealth;
 	public float maxHealth;
-	public float curStamina;
-	public float maxStamina;
+	
+	public float curEnergy;
+	public float maxEnergy;
+	
 	public int currentAmmo;
 	public int maxAmmo;
 	
-	public GraysonMechanics graysonStats;
-	public Weapon weapStats;
-	public Image healthBar;
-	public Image damageBar;
-	public Image shieldBar;
-	public Image impactBar;
-	public Image staminaBar;
 	
 	// Use this for initialization
 	void Start () {
-		//healthBar = GetComponent<Image> ();
-		//shieldBar = GetComponent<Image> ();
-		//staminaBar = GetComponent<Image> ();
-		
-		curHealth = graysonStats.getCurHealth();
-		maxHealth = graysonStats.getMaxHealth();
-		curShield = graysonStats.getCurShield ();
-		maxShield = graysonStats.getMaxShield ();
-		curStamina = graysonStats.getCurStamina ();
-		maxStamina = graysonStats.getMaxStamina ();
-		
-		initHealth = curHealth;
-		initShield = curShield;
-		
-		currentAmmo = weapStats.getCurrentAmmo ();
-		//maxAmmo = weapStats.getMaxAmmo ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if (Input.GetKeyDown (KeyCode.O)) {
-			playerTakesDamage(8f);
-		}
-		
-		shieldBar.fillAmount = curShield / maxShield;
-		impactBar.fillAmount = Mathf.MoveTowards ((initShield / maxShield), (curShield / maxShield), Time.deltaTime * .005f);
-		healthBar.fillAmount = curHealth / maxHealth;
-		damageBar.fillAmount = Mathf.MoveTowards (initHealth / maxHealth, curHealth / maxHealth, Time.deltaTime * .005f);
-		
-		initHealth = curHealth;
-		initShield = curShield;
-		
-		
 	}
-	void playerTakesDamage(float amount){
-		if(graysonStats.curShield > 0){
-			if(graysonStats.curShield < amount){
+	
+	public void damagePlayer(float damageAmount){
+		if(curShield > 0){
+			if(curShield < damageAmount){
 				float remainder = 0f;
-				remainder = amount - graysonStats.curShield;
-				graysonStats.curShield = 0;
-				graysonStats.curHealth -= remainder;
+				remainder = damageAmount - curShield;
+				curShield = 0;
+				curHealth -= remainder;
 			}else {
-				graysonStats.curShield -= amount;
+				curShield -= damageAmount;
 			}
 		}else{
-			if(graysonStats.curHealth < amount){
-				graysonStats.curHealth = 0;
+			if(curHealth < damageAmount){
+				curHealth = 0;
 			}else{
-				graysonStats.curHealth -= amount;
+				curHealth -= damageAmount;
 			}
-			
 		}
-		curHealth = graysonStats.getCurHealth();
-		curShield = graysonStats.getCurShield();
-		
+	}
+	public void healPlayer(float healAmount){
+		if ((curHealth + healAmount) >= maxHealth) {
+			curHealth = maxHealth;
+		} else {
+			curHealth += healAmount;
+		}
 	}
 }
---
+++ RebirthUnity/Assets/Scripts/UIScripts/statsDisplay.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class statsDisplay : MonoBehaviour {
+	public playerStats playerStats;
+	public Weapon weapStats;
+	
+	public Image healthBar;
+	public Image damageBar;
+	public Image shieldBar;
+	public Image impactBar;
+	public Image energyBar;
+	
+	private float initHealth;
+	private float initShield;
+	
+	// Use this for initialization
+	void Start () {
+		initHealth = playerStats.curHealth;
+		initShield = playerStats.curShield;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.O)) {
+			playerStats.damagePlayer(8f);
+		}
+		
+		//shieldBar.fillAmount = playerStats.curShield / playerStats.maxShield;
+		//impactBar.fillAmount = Mathf.MoveTowards ((initShield / playerStats.maxShield), 0, Time.deltaTime * .005f);
+		//.fillAmount = playerStats.curHealth / playerStats.maxHealth;
+		//.fillAmount = Mathf.MoveTowards (initHealth / playerStats.maxHealth, playerStats.curHealth / playerStats.maxHealth, Time.deltaTime * .005f);
+		
+		shieldBar.fillAmount = Mathf.MoveTowards ((playerStats.curShield / playerStats.maxShield), 0, Time.smoothDeltaTime * .5f);
+		
+		initHealth = playerStats.curHealth;
+		initShield = playerStats.curShield;
+	}
+}
