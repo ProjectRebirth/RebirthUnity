1e97ac9a15bc9ef08aec308cd8e63cef75d56738
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-07-03 17:41:07 -0400

    Skeleton Mechanics updates
    Added a new script to the skeleton

---
+++ RebirthUnity/Assets/Scripts/GameMechanics/EnemyMechanics/SkeletonMechanics.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkeletonMechanics : TribeMechanics {
+
+	private bool isInvincible;
+	private bool isFiring;
+	public float invincibleCoolDown;
+
+
+
+	public void activateInvincible() {
+		if (canGoInvincible ()) {
+
+		}
+	}
+
+
+	private bool canGoInvincible() {
+		return invincibleCoolDown <= 0;
+	}
+
+	protected override void Update() {
+		base.Update ();
+		invincibleCoolDown -= Time.deltaTime;
+	}
+
+	public bool getIsInvincible() {
+		return isInvincible;
+	}
+
+
+
+	public bool getIsFiring() {
+		return isFiring;
+	}
+
+}
