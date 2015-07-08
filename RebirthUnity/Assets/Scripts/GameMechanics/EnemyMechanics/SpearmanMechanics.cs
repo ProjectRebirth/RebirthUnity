c2ab9dd32d1e664a5a5458ab709f3baae19aedad
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-07-07 20:33:22 -0400

    Update to spearmanMechanics

--- RebirthUnity/Assets/Scripts/GameMechanics/EnemyMechanics/SpearmanMechanics.cs
+++ RebirthUnity/Assets/Scripts/GameMechanics/EnemyMechanics/SpearmanMechanics.cs
@@ -2,31 +2,21 @@
 using System.Collections;
 
 public class SpearmanMechanics : TribeMechanics {
-	private bool isDefending;
+	public SpearmanStats spearmanStats;
 
 	protected override void Update() {
 		base.Update ();
 
 	}
 
-
-	public void defend(bool defendPushed) {
-		isDefending = false;
-		if (checkCanDefend ()) {
-			isDefending = defendPushed;
+	public void defend(bool defendDown) {
+		if (checkCanDefend()) {
+			spearmanStats.setIsDefending(defendDown);
 		}
 	}
 
-	/**
-	 *Add any conditions that allow the sprite to defend themselves here
-	 */ 
-	public bool checkCanDefend() {
-		return !getIsRunning();
+	private bool checkCanDefend() {
+		return getIsRunning ();
 	}
 
-	public bool getIsDefending() {
-		return isDefending;
-	}
-
-	
 }
---
+++ RebirthUnity/Assets/Scripts/StatScripts/SpearmanStats.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpearmanStats : BaseStats {
+	private bool isDefending;
+
+
+
+
+	public override void takeDamage(float rawDamage) {
+		if (!isDefending) {
+			base.takeDamage(rawDamage);
+		}
+	}
+
+	public bool getIsDefending() {
+		return isDefending;
+	}
+
+	public void setIsDefending(bool isDefending) {
+		this.isDefending = isDefending;
+	}
+
+}
