f5eca13fc9765effe3c3f122e20d3657b416e01d
Johnny Mercado <johnnymercado@live.com>
2015-07-12 01:18:43 -0400

    Added Null Insignia
    Allows for no animation during stages of non-invincibility

---
+++ RebirthUnity/Assets/Scripts/AnimationTree/Insignia/SkeletonInsignia.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkeletonInsignia : MonoBehaviour {
+
+	public SkeletonMechanics skeletonMechanics;
+	public BaseStats skeletonStats;
+	public Animator animator;
+
+	// Update is called once per frame
+	void Update () {
+		animator.SetBool ("isInvincible", skeletonMechanics.getIsInvincible ());
+		animator.SetBool ("isDead", skeletonStats.getIsDead ());
+	}
+
+}
