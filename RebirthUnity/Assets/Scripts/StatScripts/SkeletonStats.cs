3b5c252df7addd265390da2495a65171e4dc7b11
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-07-15 19:26:23 -0400

    Updates to SkeletonStats
    Added invincible logic and variables to skeletonStats

--- RebirthUnity/Assets/Scripts/StatScripts/SkeletonStats.cs
+++ RebirthUnity/Assets/Scripts/StatScripts/SkeletonStats.cs
@@ -2,10 +2,28 @@
 using System.Collections;
 
 public class SkeletonStats : BaseStats {
-
+	public bool isInvincible;
+	public float chanceReflect;
+	public float invincibleCoolDown;
+	private float invincibleCoolDownTimer;
 
 	public override void takeDamage (float rawDamage) {
 
 	}
 
+	protected override void Update() {
+		base.Update ();
+		invincibleCoolDownTimer -= Time.deltaTime;
+		if (invincibleCoolDownTimer < 0f) {
+			invincibleCoolDownTimer = 0f;
+		}
+	}
+
+	public float getInvincibleTimer() {
+		return invincibleCoolDownTimer;
+	}
+
+	public bool canActivateInvincible() {
+		return invincibleCoolDownTimer <= 0f;
+	}
 }
